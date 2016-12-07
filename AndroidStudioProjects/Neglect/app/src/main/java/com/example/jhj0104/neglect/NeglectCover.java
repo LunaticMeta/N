e77858a76e08ec958dadf5a69e95e20aa4dd6dd5
jhj0104 <1214jhj.use@gmail.com>
2016-12-07 09:57:39 +0900

    Initial commit

---
+++ AndroidStudioProjects/Neglect/app/src/main/java/com/example/jhj0104/neglect/NeglectCover.java
@@ -0,0 +1,23 @@
+package com.example.jhj0104.neglect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by jhj0104 on 2016-12-07.
+ */
+
+public class NeglectCover extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cover);
+    }
+
+    public void onClick_coverLayout(View view){
+        Intent intent = new Intent(getApplicationContext(),NeglectMenu.class);
+        startActivity(intent);
+    }
+}
---
+++ AndroidStudioProjects/Neglect/app/src/main/java/com/example/jhj0104/neglect/NeglectMenu.java
@@ -0,0 +1,8 @@
+package com.example.jhj0104.neglect;
+
+/**
+ * Created by jhj0104 on 2016-12-07.
+ */
+
+public class NeglectMenu {
+}
